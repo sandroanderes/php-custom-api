3586574abd1c685d11fb43f5d1b6b6a61c3d76f4
Sandro Anderes <sandroanderes@outlook.com>
2020-08-05 10:17:44 +0200

    added db connection

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+include('config.php');
+
+class Database
+{
+    // specify your own database credentials
+    private $host = DBHOST;
+    private $db_name = DBNAME;
+    private $username = DBUSER;
+    private $password = DBPWD;
+    public $conn;
+
+    // get the database connection
+    public function getConnection()
+    {
+
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}

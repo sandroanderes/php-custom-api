46966bab6bdcec6973da03a7bc14df615ad24f0f
Sandro Anderes <sandroanderes@outlook.com>
2020-08-06 01:05:03 +0200

    changed test-license endpoint

--- license-management/index.php
+++ license-management/index.php
@@ -13,9 +13,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-// all of the endpoints start with /license
+// all of the endpoints start with /test-license
 // everything else results in a 404 Not Found
-if ($uri[2] !== 'license') {
+if ($uri[2] !== 'test-license') {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
